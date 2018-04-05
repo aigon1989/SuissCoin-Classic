7c9e607ef62d74c24fb5afb0fd9be1aad5cc5a06
AitorGonzalezMartínez <a.gonzalez.coin@gmail.com>
2018-04-05 02:06:02 +0200

    Rename Ticker SCC to SICC

--- build/moc_bitcoingui.cpp
+++ build/moc_bitcoingui.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_BitcoinGUI_t {
-    QByteArrayData data[47];
-    char stringdata0[583];
+    QByteArrayData data[50];
+    char stringdata0[642];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -60,24 +60,27 @@ QT_MOC_LITERAL(25, 291, 4), // "addr"
 QT_MOC_LITERAL(26, 296, 20), // "gotoVerifyMessageTab"
 QT_MOC_LITERAL(27, 317, 14), // "optionsClicked"
 QT_MOC_LITERAL(28, 332, 12), // "aboutClicked"
-QT_MOC_LITERAL(29, 345, 19), // "incomingTransaction"
-QT_MOC_LITERAL(30, 365, 6), // "parent"
-QT_MOC_LITERAL(31, 372, 5), // "start"
-QT_MOC_LITERAL(32, 378, 3), // "end"
-QT_MOC_LITERAL(33, 382, 13), // "encryptWallet"
-QT_MOC_LITERAL(34, 396, 11), // "cloneWallet"
-QT_MOC_LITERAL(35, 408, 12), // "exportWallet"
-QT_MOC_LITERAL(36, 421, 12), // "importWallet"
-QT_MOC_LITERAL(37, 434, 16), // "changePassphrase"
-QT_MOC_LITERAL(38, 451, 12), // "unlockWallet"
-QT_MOC_LITERAL(39, 464, 16), // "lockWalletToggle"
-QT_MOC_LITERAL(40, 481, 16), // "stakeMinerToggle"
-QT_MOC_LITERAL(41, 498, 8), // "fInitial"
-QT_MOC_LITERAL(42, 507, 13), // "inspectWallet"
-QT_MOC_LITERAL(43, 521, 12), // "repairWallet"
-QT_MOC_LITERAL(44, 534, 21), // "showNormalIfMinimized"
-QT_MOC_LITERAL(45, 556, 13), // "fToggleHidden"
-QT_MOC_LITERAL(46, 570, 12) // "toggleHidden"
+QT_MOC_LITERAL(29, 345, 17), // "trayIconActivated"
+QT_MOC_LITERAL(30, 363, 33), // "QSystemTrayIcon::ActivationRe..."
+QT_MOC_LITERAL(31, 397, 6), // "reason"
+QT_MOC_LITERAL(32, 404, 19), // "incomingTransaction"
+QT_MOC_LITERAL(33, 424, 6), // "parent"
+QT_MOC_LITERAL(34, 431, 5), // "start"
+QT_MOC_LITERAL(35, 437, 3), // "end"
+QT_MOC_LITERAL(36, 441, 13), // "encryptWallet"
+QT_MOC_LITERAL(37, 455, 11), // "cloneWallet"
+QT_MOC_LITERAL(38, 467, 12), // "exportWallet"
+QT_MOC_LITERAL(39, 480, 12), // "importWallet"
+QT_MOC_LITERAL(40, 493, 16), // "changePassphrase"
+QT_MOC_LITERAL(41, 510, 12), // "unlockWallet"
+QT_MOC_LITERAL(42, 523, 16), // "lockWalletToggle"
+QT_MOC_LITERAL(43, 540, 16), // "stakeMinerToggle"
+QT_MOC_LITERAL(44, 557, 8), // "fInitial"
+QT_MOC_LITERAL(45, 566, 13), // "inspectWallet"
+QT_MOC_LITERAL(46, 580, 12), // "repairWallet"
+QT_MOC_LITERAL(47, 593, 21), // "showNormalIfMinimized"
+QT_MOC_LITERAL(48, 615, 13), // "fToggleHidden"
+QT_MOC_LITERAL(49, 629, 12) // "toggleHidden"
 
     },
     "BitcoinGUI\0setNumConnections\0\0count\0"
@@ -89,14 +92,15 @@ QT_MOC_LITERAL(46, 570, 12) // "toggleHidden"
     "gotoAddressBookPage\0gotoReceiveCoinsPage\0"
     "gotoSendCoinsPage\0gotoSignMessageTab\0"
     "addr\0gotoVerifyMessageTab\0optionsClicked\0"
-    "aboutClicked\0incomingTransaction\0"
-    "parent\0start\0end\0encryptWallet\0"
-    "cloneWallet\0exportWallet\0importWallet\0"
-    "changePassphrase\0unlockWallet\0"
-    "lockWalletToggle\0stakeMinerToggle\0"
-    "fInitial\0inspectWallet\0repairWallet\0"
-    "showNormalIfMinimized\0fToggleHidden\0"
-    "toggleHidden"
+    "aboutClicked\0trayIconActivated\0"
+    "QSystemTrayIcon::ActivationReason\0"
+    "reason\0incomingTransaction\0parent\0"
+    "start\0end\0encryptWallet\0cloneWallet\0"
+    "exportWallet\0importWallet\0changePassphrase\0"
+    "unlockWallet\0lockWalletToggle\0"
+    "stakeMinerToggle\0fInitial\0inspectWallet\0"
+    "repairWallet\0showNormalIfMinimized\0"
+    "fToggleHidden\0toggleHidden"
 };
 #undef QT_MOC_LITERAL
 
@@ -106,7 +110,7 @@ static const uint qt_meta_data_BitcoinGUI[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      33,   14, // methods
+      34,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -114,39 +118,40 @@ static const uint qt_meta_data_BitcoinGUI[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,  179,    2, 0x0a /* Public */,
-       4,    2,  182,    2, 0x0a /* Public */,
-       6,    1,  187,    2, 0x0a /* Public */,
-       8,    0,  190,    2, 0x0a /* Public */,
-       9,    3,  191,    2, 0x0a /* Public */,
-      13,    2,  198,    2, 0x0a /* Public */,
-      17,    1,  203,    2, 0x0a /* Public */,
-      19,    0,  206,    2, 0x08 /* Private */,
-      20,    0,  207,    2, 0x08 /* Private */,
-      21,    0,  208,    2, 0x08 /* Private */,
-      22,    0,  209,    2, 0x08 /* Private */,
-      23,    0,  210,    2, 0x08 /* Private */,
-      24,    1,  211,    2, 0x08 /* Private */,
-      24,    0,  214,    2, 0x28 /* Private | MethodCloned */,
-      26,    1,  215,    2, 0x08 /* Private */,
-      26,    0,  218,    2, 0x28 /* Private | MethodCloned */,
-      27,    0,  219,    2, 0x08 /* Private */,
-      28,    0,  220,    2, 0x08 /* Private */,
-      29,    3,  221,    2, 0x08 /* Private */,
-      33,    1,  228,    2, 0x08 /* Private */,
-      34,    0,  231,    2, 0x08 /* Private */,
-      35,    0,  232,    2, 0x08 /* Private */,
-      36,    0,  233,    2, 0x08 /* Private */,
-      37,    0,  234,    2, 0x08 /* Private */,
-      38,    0,  235,    2, 0x08 /* Private */,
-      39,    0,  236,    2, 0x08 /* Private */,
-      40,    1,  237,    2, 0x08 /* Private */,
-      40,    0,  240,    2, 0x28 /* Private | MethodCloned */,
-      42,    0,  241,    2, 0x08 /* Private */,
-      43,    0,  242,    2, 0x08 /* Private */,
-      44,    1,  243,    2, 0x08 /* Private */,
-      44,    0,  246,    2, 0x28 /* Private | MethodCloned */,
-      46,    0,  247,    2, 0x08 /* Private */,
+       1,    1,  184,    2, 0x0a /* Public */,
+       4,    2,  187,    2, 0x0a /* Public */,
+       6,    1,  192,    2, 0x0a /* Public */,
+       8,    0,  195,    2, 0x0a /* Public */,
+       9,    3,  196,    2, 0x0a /* Public */,
+      13,    2,  203,    2, 0x0a /* Public */,
+      17,    1,  208,    2, 0x0a /* Public */,
+      19,    0,  211,    2, 0x08 /* Private */,
+      20,    0,  212,    2, 0x08 /* Private */,
+      21,    0,  213,    2, 0x08 /* Private */,
+      22,    0,  214,    2, 0x08 /* Private */,
+      23,    0,  215,    2, 0x08 /* Private */,
+      24,    1,  216,    2, 0x08 /* Private */,
+      24,    0,  219,    2, 0x28 /* Private | MethodCloned */,
+      26,    1,  220,    2, 0x08 /* Private */,
+      26,    0,  223,    2, 0x28 /* Private | MethodCloned */,
+      27,    0,  224,    2, 0x08 /* Private */,
+      28,    0,  225,    2, 0x08 /* Private */,
+      29,    1,  226,    2, 0x08 /* Private */,
+      32,    3,  229,    2, 0x08 /* Private */,
+      36,    1,  236,    2, 0x08 /* Private */,
+      37,    0,  239,    2, 0x08 /* Private */,
+      38,    0,  240,    2, 0x08 /* Private */,
+      39,    0,  241,    2, 0x08 /* Private */,
+      40,    0,  242,    2, 0x08 /* Private */,
+      41,    0,  243,    2, 0x08 /* Private */,
+      42,    0,  244,    2, 0x08 /* Private */,
+      43,    1,  245,    2, 0x08 /* Private */,
+      43,    0,  248,    2, 0x28 /* Private | MethodCloned */,
+      45,    0,  249,    2, 0x08 /* Private */,
+      46,    0,  250,    2, 0x08 /* Private */,
+      47,    1,  251,    2, 0x08 /* Private */,
+      47,    0,  254,    2, 0x28 /* Private | MethodCloned */,
+      49,    0,  255,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Int,    3,
@@ -167,7 +172,8 @@ static const uint qt_meta_data_BitcoinGUI[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QModelIndex, QMetaType::Int, QMetaType::Int,   30,   31,   32,
+    QMetaType::Void, 0x80000000 | 30,   31,
+    QMetaType::Void, QMetaType::QModelIndex, QMetaType::Int, QMetaType::Int,   33,   34,   35,
     QMetaType::Void, QMetaType::Bool,    7,
     QMetaType::Void,
     QMetaType::Void,
@@ -175,11 +181,11 @@ static const uint qt_meta_data_BitcoinGUI[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,   41,
+    QMetaType::Void, QMetaType::Bool,   44,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool,   45,
+    QMetaType::Void, QMetaType::Bool,   48,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -210,21 +216,22 @@ void BitcoinGUI::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 15: _t->gotoVerifyMessageTab(); break;
         case 16: _t->optionsClicked(); break;
         case 17: _t->aboutClicked(); break;
-        case 18: _t->incomingTransaction((*reinterpret_cast< const QModelIndex(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
-        case 19: _t->encryptWallet((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 20: _t->cloneWallet(); break;
-        case 21: _t->exportWallet(); break;
-        case 22: _t->importWallet(); break;
-        case 23: _t->changePassphrase(); break;
-        case 24: _t->unlockWallet(); break;
-        case 25: _t->lockWalletToggle(); break;
-        case 26: _t->stakeMinerToggle((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 27: _t->stakeMinerToggle(); break;
-        case 28: _t->inspectWallet(); break;
-        case 29: _t->repairWallet(); break;
-        case 30: _t->showNormalIfMinimized((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 31: _t->showNormalIfMinimized(); break;
-        case 32: _t->toggleHidden(); break;
+        case 18: _t->trayIconActivated((*reinterpret_cast< QSystemTrayIcon::ActivationReason(*)>(_a[1]))); break;
+        case 19: _t->incomingTransaction((*reinterpret_cast< const QModelIndex(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 20: _t->encryptWallet((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 21: _t->cloneWallet(); break;
+        case 22: _t->exportWallet(); break;
+        case 23: _t->importWallet(); break;
+        case 24: _t->changePassphrase(); break;
+        case 25: _t->unlockWallet(); break;
+        case 26: _t->lockWalletToggle(); break;
+        case 27: _t->stakeMinerToggle((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 28: _t->stakeMinerToggle(); break;
+        case 29: _t->inspectWallet(); break;
+        case 30: _t->repairWallet(); break;
+        case 31: _t->showNormalIfMinimized((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 32: _t->showNormalIfMinimized(); break;
+        case 33: _t->toggleHidden(); break;
         default: ;
         }
     }
@@ -255,13 +262,13 @@ int BitcoinGUI::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 33)
+        if (_id < 34)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 33;
+        _id -= 34;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 33)
+        if (_id < 34)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 33;
+        _id -= 34;
     }
     return _id;
 }
